4ec437eb05b658b78582274180094f29c9dd4022
hsyemfk <103497721+hsyemfk@users.noreply.github.com>
2022-08-24 15:10:57 +0430

    Add files via upload

--- Config.py
+++ Config.py
@@ -1,4 +1,4 @@
-auth = "htvtmtmsvwazhdxzcvdbzmhomtceeppd"
+auth = "xffmaicbkrrlofhuysquscwmenjpriai"
 guidgap = "g0BbWVQ04efc221bc358b350870b9808"
 admins = ["u0DP5Nj0c161d78dcac0e09ee3d9b79a","u0Ds5ni0168441041bad4f1bd9e78cbe"]
 
@@ -7,15 +7,21 @@ admins = ["u0DP5Nj0c161d78dcac0e09ee3d9b79a","u0Ds5ni0168441041bad4f1bd9e78cbe"]
 alertadmin = """
 شما ادمین شدید
 """
-alertwelcome = """سلام خوش اومدید"""
+alertwelcome = """
+سلام خوش اومدید
+"""
 bazkardangap = """
 گروه باز شد و فلان
 """
 alertremove = """
 مثلا کاربر ریم شد و فلان
 """
-alertstart = """ربات فعال """
+alertstart = """
+ربات فعال 
+"""
 alertstop = """
 ربات خاموش شد
 """
-helpbotnull = """i"""
+helpbotnull = """
+چیز راهنما
+"""
