0943f7f76db7a4476607f7810d4a5db55ff6f4d9
hsyemfk <103497721+hsyemfk@users.noreply.github.com>
2022-08-27 15:09:44 +0430

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,83 +1,16 @@
-from rubpy import Rubika
+from requests import get
 from re import findall
-from time import sleep
-import asyncio
-# rubpy
+from rubika.client import Bot
+import time
 
-bot = Rubika("htvtmtmsvwazhdxzcvdbzmhomtceeppd")
-channel_guid : str = "c0Ee9X09008b057804dadf8f941e305a"
-# داخل channel_guid شناسه‌ی کانال لینکدونی که ربات توش عضوه رو باید بزنید تا پیام هاش رو دریافت کنه و لینکارو برداره برای تبلیغ ها
-post_link: str = "https://rubika.ir/SPPORT_XSON/CHBJEGGJICBHGHF"
-# داخل post_link لینک پستتونو میزارین
+bot = Bot("AppName", auth="htvtmtmsvwazhdxzcvdbzmhomtceeppd")
 
-get_infos: list = [] # get post info
-links = []
 
-async def main():
-	t: bool = False
-	while(t != True):
-		try:
-			message_info: str = await bot.getLinkFromAppUrl(post_link)
-			global get_infos
-			get_infos.append(message_info['message_id'])
-			get_infos.append(message_info['object_guid'])
-			t: bool = True
-		except:
-			t: bool = False
-
-	t: bool = False
-	while(t!=True):
-		try:
-			messages : list = await bot.getMessagesInterval(channel_guid, await bot.getChannelLastMessageId(channel_guid))
-			t:bool=True
-		except:
-			t:bool=False
-	for msg in messages:
-		try:
-			msg = msg['text']
-			group_link = findall(r"https://rubika.ir/joing/\w{32}", msg)
-			for link in group_link:
-				links.append(link)
-		except: pass
+while True:
 	
-	while(1):
-		for link in links:
-			sleep(0)
-			t:bool=False
-			count:int=0
-			limit:int=5
-			while(count<5 and t!=True):
-				try:
-					group_guid:str= await bot.joinGroup(link)
-					group_guid: str = group_guid.get('data').get('group').get('group_guid')
-					t:bool=True
-					count += 5
-				except:
-					t:bool=False
-					count += 1
-			t:bool=False
-			count:int=0
-			limit:int=5
-			while(count<5 and t!=True):
-				try:
-				#	await bot.sendMessage(group_guid, "تبلیغ")
-					await bot.forwardMessages(get_infos[1], [get_infos[0]], group_guid)
-					print('Sended!')
-					t:bool=True
-					count+=5
-				except:
-					t:bool=False
-					count+=1
-			t:bool=False
-			count:int=0
-			limit:int=5
-			while(count<5 and t!=True):
-				try:
-					await bot.leaveGroup(group_guid)
-					t:bool=True
-					count+=5
-				except:
-					t:bool=False
-					count+=1
-
-asyncio.run(main())
+	time.sleep(12)
+	x = get("https://api.codebazan.ir/jok/").text
+	cp = f"کانال جوک ما \n @sterik_jok"
+	jok = f"{x}  \n {cp} \n "
+	bot.sendMessage("c0BGjTP0602c2cc83b3eca88a00f1702", jok)
+	print('sended')
